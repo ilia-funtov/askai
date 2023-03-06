82e2b6ff24a35ff1760c359ff025c7844603cb95
Ilia Funtov <ilia.funtov@protonmail.com>
2023-03-06 19:02:15 +0300

    Call AI providers in parallel threads

--- askaiFunc.go
+++ askaiFunc.go
@@ -13,6 +13,12 @@ var engineFuncMap = map[string]AskAIFunction{
 	"cohere": askCohere,
 }
 
+type EngineCallResult struct {
+	engineKey string
+	responses []string
+	err       error
+}
+
 func askAI(engines []string, prompt string, apiKeys map[string]string) (map[string][]string, error) {
 	if len(engines) == 0 {
 		return nil, fmt.Errorf("no AI engine found")
@@ -20,39 +26,73 @@ func askAI(engines []string, prompt string, apiKeys map[string]string) (map[stri
 
 	result := make(map[string][]string)
 
-	for _, engine := range engines {
+	processEngine := func(engine string, prompt string, apiKeys map[string]string) EngineCallResult {
 		aiProvider, aiModel, err := splitEngineName(engine)
 		if err != nil {
-			log.Warnln(err)
-		}
-
-		if aiModel == "" {
-			var exists bool
-			aiModel, exists = defaultProviderModel[aiProvider]
-			if !exists {
-				return nil, fmt.Errorf("no provider model found for %s", aiProvider)
-			}
-		}
-
-		engineFunc, exists := engineFuncMap[aiProvider]
-		if !exists {
-			return nil, fmt.Errorf("no engine found for %s", aiProvider)
+			return EngineCallResult{"", nil, err}
 		}
 
 		apiKey, exists := apiKeys[aiProvider]
 		if !exists {
-			return nil, fmt.Errorf("no API key found for %s", aiProvider)
+			return EngineCallResult{"", nil, fmt.Errorf("no API key found for %s", aiProvider)}
 		}
 
-		engineKey := fmt.Sprintf("%s:%s", aiProvider, aiModel)
-		log.Infof("Asking %s: %s", engineKey, prompt)
-		responses, err := engineFunc(prompt, aiModel, apiKey)
-		if err == nil {
-			result[engineKey] = responses
-		} else {
-			log.Errorf("Engine %s: %v", engineKey, err)
+		return callAIEngine(aiProvider, aiModel, prompt, apiKey)
+	}
+
+	if len(engines) == 1 {
+		callResult := processEngine(engines[0], prompt, apiKeys)
+		if callResult.err != nil {
+			return nil, callResult.err
+		}
+
+		result[callResult.engineKey] = callResult.responses
+		return result, nil
+	}
+
+	resultChannel := make(chan EngineCallResult)
+	processEngineAsync := func(engine string, prompt string, apiKeys map[string]string) {
+		callResult := processEngine(engine, prompt, apiKeys)
+		resultChannel <- callResult
+	}
+
+	for _, engine := range engines {
+		go processEngineAsync(engine, prompt, apiKeys)
+	}
+
+	for i := 0; i != len(engines); i++ {
+		callResult, ok := <-resultChannel
+		if ok {
+			result[callResult.engineKey] = callResult.responses
 		}
 	}
 
 	return result, nil
+}
+
+func callAIEngine(aiProvider string, aiModel string, prompt string, apiKey string) EngineCallResult {
+	if aiModel == "" {
+		var exists bool
+		aiModel, exists = defaultProviderModel[aiProvider]
+		if !exists {
+			return EngineCallResult{"", nil, fmt.Errorf("no provider model found for %s", aiProvider)}
+		}
+	}
+
+	engineKey := fmt.Sprintf("%s:%s", aiProvider, aiModel)
+
+	engineFunc, exists := engineFuncMap[aiProvider]
+	if !exists {
+		return EngineCallResult{engineKey, nil, fmt.Errorf("no engine found for %s", aiProvider)}
+	}
+
+	log.Infof("Asking %s: %s", engineKey, prompt)
+	responses, err := engineFunc(prompt, aiModel, apiKey)
+	if err == nil {
+		log.Tracef("Engine %s returned response: %v", engineKey, responses)
+	} else {
+		log.Errorf("Engine %s returned error: %v", engineKey, err)
+	}
+
+	return EngineCallResult{engineKey, responses, err}
 }
